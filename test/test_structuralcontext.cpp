a0d173eaa6deb4e85df6d29898ff631d8ef9869d
Andres Senac <andres@senac.es>
2014-12-06 21:43:55 +0000

    More tests

--- test/test_structuralcontext.cpp
+++ test/test_structuralcontext.cpp
@@ -1,14 +1,31 @@
-#include <iostream>
+#include <cassert>
 #include <ref/utils/StructuralContext.hpp>
 #include "../examples/company.hpp"
+#include <iostream>
+#include <set>
+#include <algorithm>
 
 using namespace ref;
+using namespace std;
 
 int main(int argc, char **argv)
 {
     auto companyDesc =
         example::Company::getClassDescriptorInstance();
     const StructuralContext ctx(companyDesc);
+
+    assert(ctx.getRootClass() == companyDesc);
+    assert(ctx.getAllClasses().size() == 4);
+
+    const set<string> expected = {"ref::ModelClass", "example::Department",
+                                  "example::Employee", "example::Company"};
+    const auto allClasses = ctx.getAllClasses();
+    set<string> fqns;
+    for_each(allClasses.begin(), allClasses.end(), [&](const ClassDescriptor* d)
+    {
+        fqns.insert(d->getFqn());
+    });
+    assert(fqns == expected);
 
     return 0;
 }
